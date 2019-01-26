a587562fcc83fa73c4e97685df779816da3c8c43
Michael Nix <mchl.nix@googlemail.com>
2019-01-26 19:43:16 +0100

    adds hard coded name dropdown

--- app/app/src/main/java/com/mchlnix/snookerscoreboard/ScoreBoard.java
+++ app/app/src/main/java/com/mchlnix/snookerscoreboard/ScoreBoard.java
@@ -1,7 +1,9 @@
 package com.mchlnix.snookerscoreboard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ScoreBoard extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ScoreBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_player1);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.player_names, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
